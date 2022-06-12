7c79e2b81caeef73b2c98a5b6c84d729ff672a6f
Praveen N <102466374+PraveeAlpha@users.noreply.github.com>
2022-06-12 07:42:13 +0530

    Create recursion using linkedlist.java

---
+++ recursion using linkedlist.java
@@ -0,0 +1,45 @@
+
+RECURSION:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode n=head.next;
+        ListNode nn=reverseList(n);
+        n.next=head;
+        head.next=null;
+        return nn;
+    }
+}
+
+method 2:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode rn=reverseList(head.next);
+        head.next.next=head;
+        head.next=null;
+        return rn;
+    }
+}
